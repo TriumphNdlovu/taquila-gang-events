a5f213e85f8d0db86c281c2c27577cc5b8d032d1
@Trevor240_ <realtriumphndlovu@gmail.com>
2024-10-18 17:43:26 +0200

    Refactor CORS handling and add mail send functionality

--- src/api/server.js
+++ src/api/server.js
@@ -1,8 +1,19 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
+    // Handle CORS preflight request
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+        res.status(200).end(); // Respond OK to OPTIONS requests
+        return;
+    }
+
     // Check if the request method is POST
     if (req.method === 'POST') {
+        res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust to specific origin if needed
+
         const { buyer_email } = req.body;
 
         // Configure the Nodemailer transporter
@@ -19,21 +30,11 @@ module.exports = async (req, res) => {
         try {
             await transporter.verify();
 
-            // Prepare the PDF attachment
-            // const pdfBuffer = Buffer.from(pdf_ticket, 'base64');
-
             const mailOptions = {
                 from: process.env.REACT_APP_EMAIL_USER,
                 to: buyer_email,
                 subject: 'Your Ticket for Tropical Summer Slash',
                 text: 'Hi ;),\n\nThank you for your purchase! Please find your ticket for Tropical Summer Slash attached.\n\nBest regards,\nTQG Team',
-                // attachments: [
-                //     {
-                //         filename: 'Tropical_Summer_Slash_Ticket.pdf',
-                //         content: pdfBuffer,
-                //         contentType: 'application/pdf',
-                //     },
-                // ],
             };
 
             await transporter.sendMail(mailOptions);
