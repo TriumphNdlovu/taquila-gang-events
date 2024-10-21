47f6d9d504f9e92d0c84dd5b6879926c2a85eb4a
@Trevor240_ <realtriumphndlovu@gmail.com>
2024-10-21 23:24:12 +0200

    Refactor API endpoint to remove unnecessary CORS handling and update ticket attachment method

--- src/emails/nodemailer.tsx
+++ src/emails/nodemailer.tsx
@@ -1,9 +1,28 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const sendTicketEmail = async (email: string, pdf_ticket: string | Uint8Array) => {
-    const response = await axios.post('https://tequila-gang-events.vercel.app/api/sendTicket', {
+// export const sendTicketEmail = async (email: string, pdf_ticket: string | Uint8Array) => {
+//     const response = await axios.post('https://tequila-gang-events.vercel.app/api/sendTicket', {
+//         buyer_email: email,
+//         pdf_ticket: pdf_ticket,
+//     });
+//     return response.data;
+// };
+
+export const sendTicketEmail = async (email: string, pdf_ticket_base64: any) => {
+const response = await fetch('https://us-central1-tequila-gang-tickets.cloudfunctions.net/sendTicket', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
         buyer_email: email,
-        pdf_ticket: pdf_ticket,
-    });
-    return response.data;
-};
+        pdf_ticket: pdf_ticket_base64, // Your base64 encoded PDF
+    }),
+});
+
+const result = await response.json();
+console.log(result);
+
+
+return result;
+}
