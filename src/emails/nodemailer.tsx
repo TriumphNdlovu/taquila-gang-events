04af9eede8a803cbe99606200cd01924481dad80
@Trevor240_ <realtriumphndlovu@gmail.com>
2024-10-21 14:25:03 +0200

    I am starting to go nuts

--- src/emails/nodemailer.tsx
+++ src/emails/nodemailer.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const sendTicketEmail = async (email: string, pdf_ticket: string | Uint8Array) => {
-    const response = await axios.post('/api/sendTicket', {
+    const response = await axios.post('https://tequila-gang-events.vercel.app/api/sendTicket', {
         buyer_email: email,
         pdf_ticket: pdf_ticket, // Ensure this is a base64 string if you are sending it as a PDF
     });
